b7364a154ae5034c30bab036b8a6384bac0ac1a6
Pedro E. González V <1pedrog@gmail.com>
2019-08-30 11:03:58 -0500

    Secure post[H services using JWT.

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
+const tokenChecker = require('../middlewares/token-checker')
 const controllers = require('./controller');
 
 module.exports = function (app) {
-  app.get('/api/v1/posts', controllers.list);
-  app.get('/api/v1/posts/:id', controllers.one);
-  app.post('/api/v1/posts', controllers.create);
-  app.put('/api/v1/posts/:id', controllers.update);
-  app.delete('/api/v1/posts/:id', controllers.remove);
+  app.get('/api/v1/posts', tokenChecker, controllers.list);
+  app.get('/api/v1/posts/:id', tokenChecker, controllers.one);
+  app.post('/api/v1/posts', tokenChecker, controllers.create);
+  app.put('/api/v1/posts/:id', tokenChecker, controllers.update);
+  app.delete('/api/v1/posts/:id', tokenChecker, controllers.remove);
 }
